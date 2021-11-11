c83597648d440e3e28602a43c02be2dbcb4d6d83
Fangdun Tsai <cfddream@gmail.com>
2021-11-11 10:16:29 +0800

    clap app

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+use clap::{crate_authors, crate_description, crate_version, App, crate_name};
+
+const VERSION: &str = concat!("v", crate_version!());
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new(crate_name!())
+        .version(VERSION)
+        .author(crate_authors!())
+        .about(crate_description!())
+        // .arg("-c, --config=[FILE] 'Sets a custom config file'")
+        .subcommand(
+            App::new("build")
+                .about("build sqlite db indexes from md files")
+                // .arg("-d, --debug 'Print debug information'"),
+        )
+        .subcommand(
+            App::new("server")
+                .about("run a search server for web")
+                // .arg("-d, --debug 'Print debug information'"),
+        )
+        .get_matches();
+
+    match matches.subcommand() {
+        Some(("build", m)) => {
+            println!("build {:?}", m);
+        }
+        Some(("server", m)) => {
+            println!("server {:?}", m);
+        }
+        _ => {
+        }
+    }
 }
